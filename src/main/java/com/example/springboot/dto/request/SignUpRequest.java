7988c07fab954566cdedcb5d6698327fa4774197
minhcong213274 <gathidungru@gmail.com>
2023-07-24 09:45:56 +0700

    validate

--- src/main/java/com/example/springboot/dto/request/SignUpRequest.java
+++ src/main/java/com/example/springboot/dto/request/SignUpRequest.java
@@ -12,8 +12,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class SignUpRequest {
-    @Size(min = 6, max = 32, message = "Username length must be between 6 and 32 characters")
-    @Pattern(regexp = "^[a-zA-Z0-9_]*$", message = "Username must contain only letters, numbers, and underscores")
     private String username;
     @Size(min = 6, max = 32, message = "Password length must be between 6 and 34 characters")
     private String password;
