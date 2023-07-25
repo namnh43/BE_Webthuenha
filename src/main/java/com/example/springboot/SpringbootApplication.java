97c604fb56178daed24dd58ac74ce934039f539d
mtuancsp <minhtuan267@gmail.com>
2023-07-25 08:10:51 +0700

    swagger

--- src/main/java/com/example/springboot/SpringbootApplication.java
+++ src/main/java/com/example/springboot/SpringbootApplication.java
@@ -1,15 +1,12 @@
 package com.example.springboot;
 
-import com.example.springboot.service.mail.EmailService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class SpringbootApplication {
 
-    @Autowired
-    private EmailService emailService;
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
     }
