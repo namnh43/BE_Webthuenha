ce9121aa36b5989b1ba697f947703103d7960d02
hai1710 <125845377+hai1710@users.noreply.github.com>
2023-07-31 14:31:47 +0700

    review

--- src/main/java/com/example/springboot/service/review/IReviewService.java
+++ src/main/java/com/example/springboot/service/review/IReviewService.java
@@ -1,6 +1,8 @@
 package com.example.springboot.service.review;
 
+import com.example.springboot.model.Review;
 import com.example.springboot.service.IGeneralService;
 
-public interface IReviewService extends IGeneralService {
+public interface IReviewService extends IGeneralService<Review> {
+
 }
